b588a913c70ad2b87485e6c62bba084a8ba2404f
Dartagnan Sabino <dartganan@gmail.com>
2019-08-24 03:09:56 -0300

    add post to onesignal

--- functions/hello.js
+++ functions/hello.js
@@ -23,9 +23,31 @@
  *
  */
 
+const axios = require('axios');
+
 exports.handler = (lambdaEvent, context, cb) => {
   // Get the email of the user
   const hasuraTriggerPayload = JSON.parse(lambdaEvent.body);
+
+  axios.post('https://onesignal.com/api/v1/notifications', {
+    "app_id": "0f25b644-56f3-4fa2-96bb-f5a72606ebb8",
+	"contents": {"en":"Mensagem nov2a"},
+	"headings": {"en": "oi tudo bem 2"},
+	"included_segments": ["All"],
+    /* "Authorization": 'Basic OGE0NjNiZDgtNmYxYy00MDgxLTk1NjktNmU3ZjU0ZjY1NmVm',
+    "Content-Type": 'application/x-www-form-urlencoded' */
+  },{
+    headers: {
+      "Authorization": 'Basic OGE0NjNiZDgtNmYxYy00MDgxLTk1NjktNmU3ZjU0ZjY1NmVm',
+      "Content-Type":application/x-www-form-urlencoded
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
   console.log(hasuraTriggerPayload);
   const hasuraData = hasuraTriggerPayload.event.data.new;
