0a738433a6f0590f1c653c8cb7feaa352aa13880
Dartagnan Sabino <dartganan@gmail.com>
2019-08-25 22:45:12 -0300

    push notification onesignal

---
+++ src/lambda/hello.js
@@ -0,0 +1,58 @@
+const axios = require('axios');
+
+var sendNotification = function(data) {
+  var headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Basic OGE0NjNiZDgtNmYxYy00MDgxLTk1NjktNmU3ZjU0ZjY1NmVm"
+  };
+  
+  var options = {
+    host: "onesignal.com",
+    port: 443,
+    path: "/api/v1/notifications",
+    method: "POST",
+    headers: headers
+  };
+  
+  var https = require('https');
+  var req = https.request(options, function(res) {  
+    res.on('data', function(data) {
+      console.log("Response:");
+      console.log(JSON.parse(data));
+    });
+  });
+  
+  req.on('error', function(e) {
+    console.log("ERROR:");
+    console.log(e);
+  });
+  
+  req.write(JSON.stringify(data));
+  req.end();
+};
+
+
+
+exports.handler = (event, context, callback) => {
+
+  const playload = JSON.parse(event.body);
+
+  var nameEvent = playload['event']['data']['new']['name'];
+
+  var message = { 
+    app_id: "0f25b644-56f3-4fa2-96bb-f5a72606ebb8",
+    headings: {"en": 'Faça uma pergunta na palestra'},
+    contents: {"en": nameEvent},
+    included_segments: ["All"]
+  };
+
+  console.log("ddddddddddddddddddddddddddd - ----");
+  console.log(playload['event']['data']['new']['name']);
+
+  callback(null, {
+    statusCode: 200,
+    body: 'Enviar Push rodando',
+  });
+
+  sendNotification(message);
+};
